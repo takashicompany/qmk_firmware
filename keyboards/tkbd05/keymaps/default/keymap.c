4793d55cb34f0e1cecd1608eae34bb0493ab5ade
takashicompany <t@kashi.company>
2021-06-26 18:45:02 +0900

    引数の数が違った

--- keyboards/tkbd05/keymaps/default/keymap.c
+++ keyboards/tkbd05/keymaps/default/keymap.c
@@ -36,7 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1, KC_2, KC_3, KC_4, KC_5,
         KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_H, KC_J, KC_K, KC_L, KC_ENT,
-        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
+		KC_6, KC_7, KC_8, KC_9, KC_0
     ),
     [_FN] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T,
@@ -45,7 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1, KC_2, KC_3, KC_4, KC_5,
         KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_H, KC_J, KC_K, KC_L, KC_ENT,
-        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
+		KC_6, KC_7, KC_8, KC_9, KC_0
     )
 };
 
