9ae14d83cd39b279251e47c70814ab709519b7b8
takashicompany <t@kashi.company>
2021-06-26 18:19:29 +0900

    キーマップを入れてみた

--- keyboards/tk05/keymaps/default/keymap.c
+++ keyboards/tk05/keymaps/default/keymap.c
@@ -30,12 +30,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
+        KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,
+        KC_1, KC_2, KC_3, KC_4, KC_5,
+        KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_H, KC_J, KC_K, KC_L, KC_ENT,
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC
     ),
     [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
+        KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,
+        KC_1, KC_2, KC_3, KC_4, KC_5,
+        KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_H, KC_J, KC_K, KC_L, KC_ENT,
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC
     )
 };
 
