42c277670874a8e00ff6b2092c212bf68d4dd067
takashicompany <t@kashi.company>
2021-07-03 01:03:39 +0900

    OLEDを修正

--- keyboards/endzone34/keymaps/default/keymap.c
+++ keyboards/endzone34/keymaps/default/keymap.c
@@ -87,20 +87,20 @@ void oled_task_user(void) {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        case _FN:
-            oled_write_P(PSTR("FN\n"), false);
-            break;
-        case _ADJ:
-            oled_write_P(PSTR("ADJ\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
+    // switch (get_highest_layer(layer_state)) {
+    //     case _QWERTY:
+    //         oled_write_P(PSTR("Default\n"), false);
+    //         break;
+    //     case _FN:
+    //         oled_write_P(PSTR("FN\n"), false);
+    //         break;
+    //     case _ADJ:
+    //         oled_write_P(PSTR("ADJ\n"), false);
+    //         break;
+    //     default:
+    //         // Or use the write_ln shortcut over adding '\n' to the end of your string
+    //         oled_write_ln_P(PSTR("Undefined"), false);
+    // }
 
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
