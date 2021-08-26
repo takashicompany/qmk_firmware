f94d6e6204f2c6af1f8699e030e3a3579b359da1
takashicompany <t@kashi.company>
2021-08-26 23:42:08 +0900

    ピンの数

--- keyboards/takashicompany/tkbd07/config.h
+++ keyboards/takashicompany/tkbd07/config.h
@@ -72,7 +72,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
-	#define RGBLED_NUM 5
+	#define RGBLED_NUM 10
 	#define RGBLED_SPLIT {5, 5}
 	#define RGBLIGHT_HUE_STEP 8
 	#define RGBLIGHT_SAT_STEP 8
