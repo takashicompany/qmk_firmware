c763d985f6feb6010f48fc24f7e3dcf151b64b2c
takashicompany <t@kashi.company>
2021-09-02 18:32:54 +0900

    どうやったら動くんだ？

--- keyboards/takashicompany/duplex_zone/config.h
+++ keyboards/takashicompany/duplex_zone/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 3
-#define MATRIX_COLS 3
+#define MATRIX_COLS 3 * 2
 
 /*
  * Keyboard Matrix Assignments
@@ -41,11 +41,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS { E6, B4, B5 }
-#define MATRIX_COL_PINS { D4, C6, D7 }
+#define MATRIX_COL_PINS { D4, D4, C6, C6, D7, D7 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+//#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION CUSTOM_MATRIX
 
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
