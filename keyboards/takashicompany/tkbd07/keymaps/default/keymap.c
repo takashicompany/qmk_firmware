685a704ab347a765830bb33c0ded97c5e305792f
takashicompany <t@kashi.company>
2021-09-10 21:09:01 +0900

    ちょっと直した

--- keyboards/takashicompany/tkbd07/keymaps/default/keymap.c
+++ keyboards/takashicompany/tkbd07/keymaps/default/keymap.c
@@ -78,6 +78,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+/*
 #ifdef OLED_ENABLE
 
 static void render_logo(void) {
@@ -152,3 +153,4 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
     return true;
 }
+*/
