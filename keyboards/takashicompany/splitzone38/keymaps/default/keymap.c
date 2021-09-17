328368ca714fc0ea786e7ec5363eab4831e99250
takashicompany <t@kashi.company>
2021-09-17 14:13:56 +0900

    fix keymap & rotary endoder

--- keyboards/takashicompany/splitzone38/keymaps/default/keymap.c
+++ keyboards/takashicompany/splitzone38/keymaps/default/keymap.c
@@ -110,9 +110,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     //     }
     // }
 	if (clockwise) {
-		tap_code(KC_PGDOWN);
+		tap_code(KC_MS_WH_UP);
 	} else {
-		tap_code(KC_PGUP);
+		tap_codeKC_MS_WH_DOWN);
 	}
 
     return true;
