33b294476e4e234be508fbdb3992b3316c37413f
takashicompany <t@kashi.company>
2021-09-17 18:12:10 +0900

    fix name

--- keyboards/takashicompany/splitzone38/config.h
+++ keyboards/takashicompany/splitzone38/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0007
 #define DEVICE_VER   0x0001
 #define MANUFACTURER takashicompany
-#define PRODUCT      tkbd07
+#define PRODUCT      SplitZone38
 
 /* key matrix size */
 #define MATRIX_ROWS 4*2
