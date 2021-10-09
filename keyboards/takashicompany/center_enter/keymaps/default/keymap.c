ec2e9c58c96b73596745f18a3a4a1adec039e8ed
takashicompany <t@kashi.company>
2021-10-09 20:21:34 +0900

    コードを揃えた

--- keyboards/takashicompany/center_enter/keymaps/default/keymap.c
+++ keyboards/takashicompany/center_enter/keymaps/default/keymap.c
@@ -21,16 +21,11 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // LAYOUT(
-    //     KC_1, KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I, KC_O, KC_P,
-    //     KC_2, KC_A, KC_S, KC_D, KC_F, KC_G, KC_4, KC_H, KC_J, KC_K, KC_L, KC_8,
-    //     KC_3, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_5, KC_N, KC_M, KC_6, KC_7, KC_9
-    // ),
     
     LAYOUT(
-        KC_TAB, LT(2, KC_Q), KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I, KC_O, ALT_T(KC_P),
+        KC_TAB, LT(2, KC_Q), KC_W, KC_E, KC_R, KC_T,                             KC_Y, KC_U, KC_I, KC_O, ALT_T(KC_P),
         KC_LCTL, KC_A, KC_S, LT(2, KC_D), KC_F, KC_G,               SWITCH_LANG, KC_H, KC_J, LT(2, KC_K), KC_L, KC_ENT,
-        KC_LSFT, KC_Z, GUI_T(KC_X), ALT_T(KC_C), LT(1, KC_V), KC_B, KC_SPC,  KC_N, LT(1, KC_M), KC_COMM, CTL_T(KC_DOT), KC_BSPC
+        KC_LSFT, KC_Z, GUI_T(KC_X), ALT_T(KC_C), LT(1, KC_V), KC_B, KC_SPC,      KC_N, LT(1, KC_M), KC_COMM, CTL_T(KC_DOT), KC_BSPC
     ),
 
     LAYOUT(
@@ -50,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
         KC_TRNS, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, RGB_M_T,   KC_TRNS, KC_F11, KC_F12, KC_CAPS, KC_NO, KC_NO
      )
-    
 };
+
 bool is_lang1 = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -67,10 +62,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     register_code(KC_LANG2);
                     is_lang1 = true;
                 }
-                
-            } else {
-                // when keycode QMKBEST is released
-            }
             break;
         case KC_LANG1:
             if (record->event.pressed) {
