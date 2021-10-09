f169e038cdcd6ef836665212cea406d22d9865d1
takashicompany <t@kashi.company>
2021-10-09 21:26:01 +0900

    名前を変更

--- keyboards/takashicompany/center_enter/config.h
+++ keyboards/takashicompany/center_enter/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0012
 #define DEVICE_VER   0x0001
 #define MANUFACTURER takashicompany
-#define PRODUCT      center_enter
+#define PRODUCT      Center x Enter
 
 /* key matrix size */
 #define MATRIX_ROWS 3
