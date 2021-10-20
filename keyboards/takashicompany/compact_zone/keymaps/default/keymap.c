ad8a0042a93416ed032adb26cb280872dac2340c
takashicompany <t@kashi.company>
2021-10-20 15:49:48 +0900

    ちょっと修正

--- keyboards/takashicompany/compact_zone/keymaps/default/keymap.c
+++ keyboards/takashicompany/compact_zone/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_ENT,                                       KC_TAB, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
         KC_CAPS, KC_A, KC_S, LT(2, KC_D), KC_F, KC_G,                                       KC_RCTL, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,                                      KC_RSFT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RO,
-        TG(2), LT(3 ,KC_PSCR), KC_LCTL, KC_LGUI, LALT_T(KC_LANG2), LSFT_T(KC_TAB), KC_SPC,  LT(3 ,KC_PSCR), KC_SPC, LT(1, KC_LANG1), KC_RALT, KC_RGUI, MO(3), KC_DEL
+        TG(2), LT(3 ,KC_PSCR), KC_LCTL, KC_LGUI, LSFT_T(KC_TAB), LALT_T(KC_LANG2), KC_SPC,  LT(3 ,KC_PSCR), KC_SPC, LT(1, KC_LANG1), KC_RALT, KC_RGUI, MO(3), KC_DEL
     ),
 
     LAYOUT(
