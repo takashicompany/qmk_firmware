04ea01713a56cfb42d1c2b03d529dc1ef674bd30
takashicompany <t@kashi.company>
2021-10-25 21:33:11 +0900

    名前を変更

--- keyboards/takashicompany/radialex/config.h
+++ keyboards/takashicompany/radialex/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0019
 #define DEVICE_VER   0x0001
 #define MANUFACTURER takashicompany
-#define PRODUCT      radialex
+#define PRODUCT      Radialex
 
 /* key matrix size */
 #define MATRIX_ROWS 7
