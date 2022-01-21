05072d0e81db39b740abad03577700edbdef26ed
takashicompany <t@kashi.company>
2022-01-21 18:39:26 +0900

    minizone

--- keyboards/takashicompany/minizone/config.h
+++ keyboards/takashicompany/minizone/config.h
@@ -6,15 +6,15 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x7463 // tc = takashicompany
+#define PRODUCT_ID   0x0021
 #define DEVICE_VER   0x0001
 #define MANUFACTURER takashicompany
-#define PRODUCT      minizone
+#define PRODUCT      miniZONE
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 10
 
 /*
  * Keyboard Matrix Assignments
@@ -26,8 +26,8 @@
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, D4, C6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
