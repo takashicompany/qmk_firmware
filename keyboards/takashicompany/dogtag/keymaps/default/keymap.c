23247210a3698e0020c30f71113e6abaf5693d57
takashicompany <t@kashi.company>
2022-03-27 23:42:38 +0900

    fix keymap

--- keyboards/takashicompany/dogtag/keymaps/default/keymap.c
+++ keyboards/takashicompany/dogtag/keymaps/default/keymap.c
@@ -18,8 +18,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LAYOUT(
-        LT(1, KC_ESC), KC_Q, KC_W, KC_E,                 KC_Y, KC_U, KC_I, KC_O,
-        KC_LSFT,       KC_A, KC_S, KC_D, KC_SPC, KC_TAB, KC_H, KC_J, KC_K, KC_L
+        LT(1, KC_Q),  KC_W, KC_E, KC_R,                 KC_Y, KC_U, KC_I, KC_O,
+        LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_SPC, KC_ENT, KC_H, KC_J, KC_K, KC_L
     ),
 
     LAYOUT(
