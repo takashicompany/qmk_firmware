6467390f7ba47d3a61bcbeb646167c6b1505bcc9
takashicompany <t@kashi.company>
2022-09-22 00:56:23 +0900

    fix oled logo

--- keyboards/takashicompany/meicb/keymaps/via/keymap.c
+++ keyboards/takashicompany/meicb/keymaps/via/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_P0,   KC_P1,   KC_P2,   KC_P3
+    LAYOUT(
+        KC_A, KC_B, KC_C, KC_D
     )
 };
