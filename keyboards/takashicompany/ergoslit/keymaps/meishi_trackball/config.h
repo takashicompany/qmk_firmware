523e9c453f05e8224b60145b1c9ae9b4e76a2437
takashicompany <t@kashi.company>
2022-12-22 00:12:19 +0900

    LAYER

--- keyboards/takashicompany/ergoslit/keymaps/meishi_trackball/config.h
+++ keyboards/takashicompany/ergoslit/keymaps/meishi_trackball/config.h
@@ -5,11 +5,10 @@
 
 #include "config_common.h"
 
-/* Limit the maximum layer number to 8 */
-#define LAYER_STATE_8BIT
-
 #define ADNS5050_SCLK_PIN D0
 #define ADNS5050_SDIO_PIN D1
 #define ADNS5050_CS_PIN D2
 
 #define MEISHI_TRACKBALL
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
