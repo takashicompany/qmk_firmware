46de041e4747310ee2b124a16cb27ac0ab5e6d2a
takashicompany <t@kashi.company>
2023-01-14 12:18:19 +0900

    不要なフォルダを削除

--- keyboards/chon_chee/su120/old/rules.mk
+++
@@ -1,3 +0,0 @@
-# This file intentionally left blank
-MCU = atmega32u4
-BOOTLOADER = caterina
--- keyboards/chon_chee/su120/old/su120.c
+++
@@ -1,4 +0,0 @@
-// Copyright 2023 takashicompany (@takashicompany)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#include "su120.h"
