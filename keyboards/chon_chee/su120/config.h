b10c9957daadd083a23661c99b68afb714bc880e
takashicompany <t@kashi.company>
2023-01-15 15:35:16 +0900

    修正

--- keyboards/chon_chee/su120/config.h
+++ keyboards/chon_chee/su120/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* key matrix size */
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 5 * 2
 #define MATRIX_COLS 7
 
 /*
--- keyboards/chon_chee/su120/rules.mk
+++ keyboards/chon_chee/su120/rules.mk
@@ -16,5 +16,5 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-# SPLIT_KEYBOARD=yes
+SPLIT_KEYBOARD=yes
 LTO_ENABLE = yes
