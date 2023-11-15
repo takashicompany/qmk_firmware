677d24fbeab3fb01b18072f7df6ae497e7f16d0e
takashicompany <t@kashi.company>
2023-11-16 00:11:24 +0900

    rookey

---
+++ keyboards/takashicompany/rookey/keymaps/default/keymap.c
@@ -0,0 +1,31 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "quantum.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R,
+        KC_A, KC_S, KC_D, KC_F,
+        KC_V,
+        KC_NO, KC_NO
+    )
+};
+
+
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    
+    if (!encoder_update_user(index, clockwise)) {
+      return false;
+    }
+    
+    if (clockwise) {
+        tap_code(KC_1);
+    } else {
+        tap_code(KC_2);
+    }
+    
+    return true;
+}
