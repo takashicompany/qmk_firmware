e2d32adbfb674da40660be166466cc3b1578c413
takashicompany <t@kashi.company>
2024-01-04 01:36:19 +0900

    アナログスティックの実験

--- keyboards/takashicompany/rookey/keymaps/default/keymap.c
+++ keyboards/takashicompany/rookey/keymaps/default/keymap.c
@@ -3,6 +3,9 @@
 
 #include QMK_KEYBOARD_H
 
+#include "print.h"
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         LT(1, KC_Q), KC_W, KC_E, KC_R,
@@ -22,3 +25,20 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [1] =   { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) }
 };
 #endif
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  // debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (mouse_report.x != 0 || mouse_report.y != 0) {
+        // 出力するコード
+        uprintf("X: %d, Y: %d\n", mouse_report.x, mouse_report.y);
+    }
+
+    return mouse_report;
+}
--- keyboards/takashicompany/rookey/rules.mk
+++ keyboards/takashicompany/rookey/rules.mk
@@ -1,1 +1,6 @@
 # This file intentionally left blank
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = analog_joystick
+
+CONSOLE_ENABLE = yes
