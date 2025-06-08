3b0183c19ee484f0f68970063935957783e19d8e
takashicompany <t@kashi.company>
2025-06-08 15:03:21 +0900

    fix

--- keyboards/green_keys/gravity_45/keymaps/via_takashicompany/keymap.c
+++ keyboards/green_keys/gravity_45/keymaps/via_takashicompany/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(7, KC_Q), KC_W, KC_E, LT(6, KC_R), KC_T, KC_NO, KC_NO, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_NO, RGB_TOG, KC_NO, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC,
-        /* 4 行目はそのまま */ KC_LCTL, KC_LGUI, LALT_T(KC_LNG2), LSFT_T(KC_TAB), LT(2, KC_SPC), LT(1, KC_LNG1), KC_RALT, KC_RGUI
+        /* 4 行目はそのまま */ KC_LGUI, LALT_T(KC_LNG2), LSFT_T(KC_TAB), KC_NO, LT(2, KC_SPC), LT(1, KC_LNG1), KC_RALT, KC_RGUI
     ),
 
 /* ───────── Layer 1 ───────── */
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(7, KC_Q), KC_W, KC_E, LT(6, KC_R), KC_T, KC_TRNS, KC_TRNS, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_TRNS, KC_TRNS, KC_TRNS, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_TRNS, KC_TRNS, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC,
-        /* 4 行目はそのまま */ KC_LCTL, KC_LGUI, LALT_T(KC_LNG2), LSFT_T(KC_TAB), LT(5, KC_SPC), LT(4, KC_LNG1), KC_RALT, KC_RGUI
+        /* 4 行目はそのまま */KC_LGUI, LALT_T(KC_LNG2), LSFT_T(KC_TAB), KC_NO, LT(5, KC_SPC), LT(4, KC_LNG1), KC_RALT, KC_RGUI
     ),
 
 /* ───────── Layer 4 ───────── */
